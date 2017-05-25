5f76be622e95c0135a7e134f75e68b0de8095643
bshafer93 <bshafer93@gmail.com>
2017-05-24 19:22:05 -0700

    1

--- libs/dataOBJ.go
+++ libs/dataOBJ.go
@@ -244,7 +244,7 @@ func  Login(){
 	db := OpenDB()
 
 	var user UserCheck
-	row, err := db.Query("SELECT username,password, email FROM login WHERE username=?", "Brent")
+	row, err := db.Query("SELECT username,email FROM login WHERE username=?", "Brent")
 	fmt.Println(row,"This is a row")
 	if err != nil {
 		log.Fatal(err)
